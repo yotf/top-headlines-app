83551556c80feedc044a463c8fbf90adc97acdf0
tara.petric <tp@helveticode.ch>
2023-11-29 15:05:40 +0100

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,29 @@ type HeaderProps = {
 };
 const Header: FC<HeaderProps> = ({ selectedCountry, handleCountryChange }) => {
   return (
-    <header>
-      <nav>
+    <header className="flex justify-between items-center">
+      <nav className="flex justify-between gap-4">
         <Link to={`/top-news`}>Top News</Link>
         <Link to={`/categories`}>Categories</Link>
         <Link to={`/search`}>Search</Link>
       </nav>
       <div className="language-select">
         <button
-          className={selectedCountry === "us" ? "active" : ""}
+          className={
+            selectedCountry === "us"
+              ? "text-red-400 border-b-2 border-red-400"
+              : ""
+          }
           onClick={() => handleCountryChange("us")}
         >
           US
         </button>
         <button
-          className={selectedCountry === "gb" ? "active" : ""}
+          className={
+            selectedCountry === "gb"
+              ? "text-red-400 border-b-2 border-red-400"
+              : ""
+          }
           onClick={() => handleCountryChange("gb")}
         >
           GB
