d8ebfa1a475a8c953e50b849bf445bbb05ea6811
tara.petric <tp@helveticode.ch>
2023-11-30 13:18:12 +0100

    add category page

--- src/pages/SingleCategoryPage.tsx
+++ src/pages/SingleCategoryPage.tsx
@@ -1,5 +1,19 @@
+import { useOutletContext, useParams } from "react-router-dom";
+import useNews from "../hooks/useNews";
+import { NewsCategory } from "../types";
+import NewsContainer from "../components/NewsContainer";
+
 const SingleCategoryPage = () => {
-  return <div></div>;
+  const selectedCountry = useOutletContext<"us" | "gb">();
+  const { category } = useParams() as { category: NewsCategory };
+  const { data: articles, isLoading } = useNews(selectedCountry, category);
+
+  return (
+    <div>
+      <h1>{category}</h1>
+      <NewsContainer articles={articles} isLoading={isLoading} />
+    </div>
+  );
 };
 
 export default SingleCategoryPage;
